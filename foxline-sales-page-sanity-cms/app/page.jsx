576fcd56247b5ac1e685487d42da90a6f2e9a742
foxlineprojects-dotcom <foxline.projects@gmail.com>
2025-10-25 06:23:17 +0600

    Update page.jsx

--- foxline-sales-page-sanity-cms/app/page.jsx
+++ foxline-sales-page-sanity-cms/app/page.jsx
@@ -24,7 +24,7 @@ const BlogPreviewWrapper = dynamic(
 
 export default function FoxlineClientWrapper() {
   return (
-    <div className="pb-10">
+    <div className="pb-20">
       <header className="flex justify-between items-center p-6 max-w-5xl mx-auto">
         <Image src={Logo} alt="Foxline" className="w-[120px]" />
         <DarkModeToggle />
@@ -45,4 +45,5 @@ export default function FoxlineClientWrapper() {
     </div>
   );
 }
+
 
